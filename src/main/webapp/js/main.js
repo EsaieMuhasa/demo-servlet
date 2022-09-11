bce9ca5acb6c525c8a0a848858f7abf4dc395733
Esaie Muhasa <esaiemuhasa.dev@gmail.com>
2022-09-11 15:45:40 +0200

    Mis en jours du select: formulaire de selection de port COM

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,9 +1,12 @@
 /**
  * callback de reception du message
- * @param {string} message message recue depui le serveur
+ * @param {MessageEvent} message message recue depui le serveur
  */
 function onMessage (message) {
-    console.log(message);
+    var data = message.data;
+
+    var json = JSON.parse(data);
+    updateSelecte(json);
 } 
 
 /**
@@ -24,6 +27,38 @@ function onClose (event) {
 
 function onError (error) {
     console.log(error);
+}
+
+/**
+ * mise en jours du select contenant les ports serie (COM port) connecter sur le serveur
+ * @param {Object} data 
+ * @param {string} data.currentPort le port sur lequel le serveur est actuelement connectee
+ * @param {string[]} data.port collection des ports disponible sur le serveur
+ */
+function updateSelecte (data) {
+    var ports = data.ports;
+    var selected = data.currentPort ==  ''? null : data.currentPort;
+
+    var select = document.getElementsByTagName('select')[0];
+    select.innerHTML = '<option value="none">--Aucun port--</option>';
+
+    for (let i = 0; i < ports.length; i++) {
+        const portName = ports[i];
+        
+        var option= document.createElement('option');
+        option.setAttribute('value', portName);
+        option.appendChild(document.createTextNode(portName));
+
+        select.appendChild(option);
+
+        if(selected == null){
+            continue;
+        }
+
+        if (selected == portName) {
+            option.setAttribute('selected', 'selected');
+        }
+    }
 }
 
 (function main () {
